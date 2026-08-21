4817cd4f375a60d5af45aba55be27adae8403673
mustafa1234466 <musfatdon@gmail.com>
2026-08-21 14:54:32 +0500

    Fix function crash issue

--- server.js
+++ server.js
@@ -8,9 +8,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 module.exports = app;
